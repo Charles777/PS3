8dbf01298d72aad092b31eedb4b7231c6d9258a6
Charles E Collins <charles@udel.edu>
2015-09-20 15:31:01 -0400

    PS3

---
+++ PS3/src/mainPackage/Account.java
@@ -0,0 +1,237 @@
+package mainPackage;
+import java.util.Date;
+
+/**
+ * @author Owner
+ *
+ */
+public class Account {
+	private int id;
+	private double balance;
+	private double annualInterestRate;
+	private Date dateCreated;
+	
+	/**
+	 * No arg constructor defaults to an id and balance of 0. 
+	 * The date is still accurate.
+	 */
+	public Account(){
+		id = 0;
+		balance = 0;
+		dateCreated = new Date();
+
+	}
+	
+	/**
+	 * Passing an id and balance allows them to be assigned on initial construction. 
+	 * The date is recorded automatically.
+	 * @param idin
+	 * @param balancein
+	 */
+	public Account(int idin, double balancein){
+		id = idin;
+		balance = balancein;
+		dateCreated = new Date();	
+		
+	}
+	
+	/**
+	 * @return this.id
+	 */
+	public int getId(){
+		return this.id;
+		
+	}
+	
+	/**
+	 * @param idin
+	 */
+	public void setId(int idin){
+		this.id = idin;
+		
+	}
+	
+	/**
+	 * @param a
+	 * @return a.id
+	 */
+	public static int getId(Account a){
+		return a.id;
+		
+	}
+	
+	/**
+	 * @param a
+	 * @param idin
+	 */
+	public static void setId(Account a, int idin){
+		a.id = idin;
+		
+	}
+	
+	/**
+	 * @return id
+	 */
+	public double getBalance(){
+		return this.balance;
+		
+	}
+	
+	/**
+	 * @param bal
+	 */
+	public void setBalance(double bal){
+		this.balance = bal;
+		
+	}
+	
+	/**
+	 * @param a
+	 * @return a.balance
+	 */
+	public static double getBalance(Account a){
+		return a.balance;
+		
+	}
+	
+	/**
+	 * @param a
+	 * @param bal
+	 */
+	public static void setBalance(Account a, double bal){
+		a.balance = bal;
+		
+	}
+	
+	/**
+	 * @return a.annualInterestRate
+	 */
+	public double getAnnualInterestRate() {
+		return this.annualInterestRate;
+		
+	}
+	
+	/**
+	 * @param air
+	 */
+	public void setAnnualInterestRate(double air){
+		this.annualInterestRate = air;
+		
+	}
+	
+	/**
+	 * @param a
+	 * @return a.annualInterestRate
+	 */
+	public static double getAnnualInterestRate(Account a){
+		return a.annualInterestRate;
+		
+	}
+	
+	/**
+	 * @param a
+	 * @param air
+	 */
+	public static void setAnnualInterestRate(Account a, double air){
+		a.annualInterestRate = air;
+		
+	}
+	
+	/**
+	 * @return this.dateCreated
+	 */
+	public Date getDateCreated(){
+		return this.dateCreated;
+		
+	}
+	
+	/**
+	 * @param a
+	 * @return a.dateCreated
+	 */
+	public static Date getDateCreated(Account a){
+		return a.dateCreated;
+		
+	}
+	
+
+	/**
+	 * Calculates the monthly interest rate from the annual rate
+	 * @return mir the monthly interest rate
+	 */
+	public double getMonthlyInterestRate(){
+		double mir = this.getAnnualInterestRate()/12;
+		return mir;
+		
+	}
+	
+	/**
+	 * @param a
+	 * @return mir the monthly interest rate
+	 */
+	public static double getMonthlyInterestRate(Account a){
+		double mir = a.getAnnualInterestRate()/12;
+		return mir;
+		
+	}
+	
+	/**
+	 * Subtracts the given amount from the account balance.
+	 * @param amount
+	 */
+	public void withdraw(double amount) throws InsufficientFundsException 
+	{
+	      if(amount <= this.balance)
+	      {
+	    	  this.balance = this.balance - amount;
+	      }
+	      else
+	      {
+	         double needs = amount - this.balance;
+	         throw new InsufficientFundsException(needs);
+	      }
+	}
+	
+	/**
+	 * Subtracts the given amount from the account balance.
+	 * @param a
+	 * @param amount
+	 */
+	public static void withdraw(Account a, double amount) throws InsufficientFundsException
+	{
+	      if(amount <= a.balance)
+	      {
+	    	  a.balance = a.balance - amount;
+	      }
+	      else
+	      {
+	         double needs = amount - a.balance;
+	         throw new InsufficientFundsException(needs);
+	      }
+		
+	}
+	
+	/**
+	 * Adds the given amount to the account balance.
+	 * @param amount
+	 */
+	public void deposit(double amount){
+		this.balance = this.balance + amount;
+		
+	}
+	
+	/**
+	 * Adds the given amount to the account balance.
+	 * @param a
+	 * @param amount
+	 */
+	public static void deposit(Account a, double amount){
+		a.balance = a.balance + amount;
+		
+	}
+	
+	
+	
+	
+	
+}
---
+++ PS3/src/mainPackage/InsufficientFundsException.java
@@ -0,0 +1,22 @@
+package mainPackage;
+
+public class InsufficientFundsException extends Exception
+{
+   /**
+	 * 
+	 */
+	private static final long serialVersionUID = -2973071757403054772L;
+private double amount;
+   /**
+    * Create an instance of the InsufficientFundsException class, to be thrown to the caller
+    * @param amount
+    */
+   public InsufficientFundsException(double amount)
+   {
+      this.amount = amount;
+   } 
+   public double getAmount()
+   {
+      return amount;
+   }
+}
